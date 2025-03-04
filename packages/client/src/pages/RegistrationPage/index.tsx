da11def3e276ed759ea6c72277245926e8ff6668
unknown <alsegusev@greenatom.ru>
2025-03-04 13:42:23 +0300

    feat: Добавил форму регистрации

--- packages/client/src/pages/RegistrationPage/index.tsx
+++ packages/client/src/pages/RegistrationPage/index.tsx
@@ -1,9 +1,51 @@
-import { MainLayout } from '@/widgets'
+import { FC } from 'react'
+import { Button, Form, Input, Flex, Typography } from 'antd'
+import { NavLink } from 'react-router-dom'
+import { RouterPaths } from '@/shared/router'
 
-export const RegistrationPage = () => {
+const { Title } = Typography
+
+export const RegistrationPage: FC = () => {
+  const onFinish = (values: unknown) => {
+    console.log('введенные данные', values)
+  }
+
   return (
-    <MainLayout isHeader={false}>
-      <div>Registration Page</div>
-    </MainLayout>
+    <Flex justify="center" align="center" style={{ height: '100%' }} vertical>
+      <Title>Регистрация</Title>
+      <Form
+        name="registration"
+        style={{ minWidth: 400 }}
+        onFinish={onFinish}
+        layout="vertical">
+        <Form.Item name="first_name" label="Имя">
+          <Input name="first_name" />
+        </Form.Item>
+        <Form.Item name="second_name" label="Фамилия">
+          <Input name="second_name" />
+        </Form.Item>
+        <Form.Item name="login" label="Логин">
+          <Input name="login" />
+        </Form.Item>
+        <Form.Item name="email" label="Почта">
+          <Input name="email" />
+        </Form.Item>
+        <Form.Item name="phone" label="Телефон">
+          <Input name="phone" />
+        </Form.Item>
+        <Form.Item name="password" label="Пароль">
+          <Input.Password name="password" />
+        </Form.Item>
+        <Form.Item name="password2" label="Повторите пароль">
+          <Input.Password name="password2" />
+        </Form.Item>
+        <Form.Item>
+          <Button block type="primary" htmlType="submit">
+            Зарегистрироваться
+          </Button>
+          Уже есть аккаунт? <NavLink to={RouterPaths.login}>Войти</NavLink>
+        </Form.Item>
+      </Form>
+    </Flex>
   )
 }
