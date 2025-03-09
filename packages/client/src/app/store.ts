a1b0be63087ff7b5599e6ff3bab1dc27dc2ca9e5
PaulLyu <polina.lyubeznova@geogracom.com>
2025-03-10 02:01:48 +0300

    feat: Инициализировала store и StoreProvider

--- packages/client/src/app/store.ts
+++ packages/client/src/app/store.ts
@@ -1,9 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import UserReducer from '@/entities/User/slice'
 
-const store = configureStore({
-  reducer: {
-    // user: userReducer,
-  },
+const reducer = combineReducers({
+  user: UserReducer,
+})
+
+// @ts-expect-error типы any
+const rootReducer = (state, action) => {
+  if (action.type === 'reset') {
+    state = undefined
+  }
+  return reducer(state, action)
+}
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 })
 
 export type RootState = ReturnType<typeof store.getState>
