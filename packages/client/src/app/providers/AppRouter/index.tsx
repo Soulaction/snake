6d96a4f1593a9d824ba222d8fc3ff842fda71ca6
AlexGusev19 <alsegusev@greenatom.ru>
2025-03-09 12:06:51 +0300

    refactor: добавил условный рендеринг для заголовка

--- packages/client/src/app/providers/AppRouter/index.tsx
+++ packages/client/src/app/providers/AppRouter/index.tsx
@@ -6,11 +6,11 @@ import type { FC } from 'react'
 
 export const AppRouter: FC = () => (
   <Routes>
-    <Route key={RouterPaths.login} path={RouterPaths.login} element={<LoginPage />}/>
-    <Route key={RouterPaths.registration} path={RouterPaths.registration} element={<RegistrationPage />}/>
-    <Route key={RouterPaths['not-found']} path={RouterPaths['not-found']} element={<NotFoundPage />}/>
-    <Route key={RouterPaths.game} path={RouterPaths.game} element={<GamePage />}/>
     <Route key="layout" element={<MainLayout />}>
+      <Route key={RouterPaths.login} path={RouterPaths.login} element={<LoginPage />}/>
+      <Route key={RouterPaths.registration} path={RouterPaths.registration} element={<RegistrationPage />}/>
+      <Route key={RouterPaths['not-found']} path={RouterPaths['not-found']} element={<NotFoundPage />}/>
+      <Route key={RouterPaths.game} path={RouterPaths.game} element={<GamePage />}/>    
       <Route key={RouterPaths.main} path={RouterPaths.main} element={<MainPage />}/>
       <Route key={RouterPaths.leaderboard} path={RouterPaths.leaderboard} element={<LeaderboardPage />}/>
       <Route key={RouterPaths.forum} path={RouterPaths.forum} element={<ForumPage />}/>
--- packages/client/src/widgets/layouts/MainLayout/index.tsx
+++ packages/client/src/widgets/layouts/MainLayout/index.tsx
@@ -2,14 +2,20 @@ import type { FC } from 'react'
 import { Layout } from 'antd'
 import { Header } from '@/widgets/Header'
 import styles from './MainLayout.module.css'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import { RouterPaths } from '@/shared/router'
 
 const { Content } = Layout
 
 export const MainLayout: FC = () => {
+  const { pathname } = useLocation();
+  const isShowHeader = pathname !== RouterPaths.login && pathname !== RouterPaths.registration;
+
+  console.log({location, isShowHeader});
+  
   return (
     <Layout className={styles['layout']}>
-      <Header />
+      {isShowHeader && <Header />}
       <Content className={styles['content']}>
         <div className={styles['card']}><Outlet /></div>
       </Content>
