bd6ead019118e3caab3791d63585fd93890c996a
Eugene Egorov <egorovzhenya@gmail.com>
2025-03-19 23:16:04 +0300

    feat: добавил кнопку включения полноэкранного режима

--- packages/client/src/features/GameInfo/ui/GameInfo.tsx
+++ packages/client/src/features/GameInfo/ui/GameInfo.tsx
@@ -6,12 +6,14 @@ import { StatusGame } from '@/widgets/Game/model/types'
 import { setStatusGame } from '@/widgets/Game/model/gemeSlice'
 import { formatTimeFromSecond } from '@/features/GameInfo/lib/formatedTimeFromMS'
 import { store } from '@/app/store'
+import { toggleFullscreen } from '@/shared/lib/webApi'
 
 export const GameInfo: FC = () => {
   const timeGameSecond = useRef<number>(0)
   const intervalId = useRef<ReturnType<typeof setInterval>>()
   const [timeGame, setTimeGame] = useState<string>(formatTimeFromSecond(0))
   const [isPause, setIsPause] = useState<boolean>(false)
+  const [textContent, toggleFullScreen] = toggleFullscreen()
 
   const score = useAppSelector(state => state.game.score)
   const statusGame = useAppSelector(state => state.game.statusGame)
@@ -109,6 +111,7 @@ export const GameInfo: FC = () => {
         <Button color="primary" variant="outlined" onClick={togglePause}>
           {isPause ? 'Продолжить' : 'Пауза'}
         </Button>
+        <Button onClick={toggleFullScreen}>{textContent}</Button>
         <Popconfirm
           placement="topRight"
           title="Вы действительно хотите покинуть игру?"
