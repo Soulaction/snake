89aa616235690682076874751ca314e56ffd4138
Dmitry <ds-8.5@bk.ru>
2025-04-20 16:44:16 +0300

    fix: поправил конфиг jest на cjs

--- packages/client/jest.config.cjs
+++ packages/client/jest.config.cjs
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-export default {
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
