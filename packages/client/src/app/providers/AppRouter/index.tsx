4472c2b08a92292223cefd1fc6784c6473b54b1f
AlexGusev19 <alexgusev1990@gmail.com>
2025-04-26 10:38:08 +0300

    feat: добавил переключение конфига в ConfigProvider

--- packages/client/src/app/providers/AppRouter/index.tsx
+++ packages/client/src/app/providers/AppRouter/index.tsx
@@ -1,11 +1,21 @@
-import { FC } from 'react'
+import { FC, useMemo } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { getRouter } from '@/shared/router'
 import { App, ConfigProvider, theme } from 'antd'
+import { useAppSelector } from '@/shared/hooks'
 
 export const AppRouter: FC = () => {
+  const currentTheme = useAppSelector(state => state.user.theme)
+
+  const config = useMemo(() => {
+    return {
+      algorithm:
+        currentTheme === 'light' ? theme.defaultAlgorithm : theme.darkAlgorithm,
+    }
+  }, [currentTheme])
+
   return (
-    <ConfigProvider theme={{ algorithm: theme.defaultAlgorithm }}>
+    <ConfigProvider theme={config}>
       <App>
         <RouterProvider router={getRouter()} />
       </App>
