74288c6682f486f0007251bfcc27e7439762cad1
Dmitry Bolshakov <60438778+Soulaction@users.noreply.github.com>
2025-05-17 22:40:32 +0300

    fix: env

--- packages/client/src/shared/constants/api/index.ts
+++ packages/client/src/shared/constants/api/index.ts
@@ -1,14 +1,15 @@
 const isDev = process.env.NODE_ENV === 'dev'
-console.log(process.env)
+console.log('++++++++++++++++++++++++++++')
+console.log(import.meta.env)
 export const apiYandex = `${
   isDev
     ? import.meta.env.VITE_INTERNAL_SSR_SERVER_URL
-    : process.env.INTERNAL_SERVER_URL
+    : import.meta.env.VITE_INTERNAL_SERVER_URL
 }/api/v2`
 export const apiSnakeServer = `${
   isDev
     ? import.meta.env.VITE_INTERNAL_SNAKE_SERVER_URL
-    : process.env.INTERNAL_SERVER_URL
+    : import.meta.env.VITE_INTERNAL_SERVER_URL
 }/snake-api/v2`
 export const resourcesYandex = apiYandex + '/resources'
 export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI
